41bea1ec6194ced4e404fa8501e1a2750d0ca535
Unknown <therobotprogrammer@gmail.com>
2018-07-15 13:31:00 +0530

    training now continues even if csv was not saved due to keras bug

--- utils/model_load_and_save.py
+++ utils/model_load_and_save.py
@@ -64,9 +64,12 @@ class model_load_and_save:
 
             if saved_model_filename != None:
                 try:    
-                    self.model = load_model(saved_model_filename)                    
-                    log_pd = pd.read_csv(saved_csv_save_file, header = 0)  
-                    self.initial_epoch = 1 + log_pd['epoch'].iloc[-1]                    
+                    self.model = load_model(saved_model_filename)      
+                    if not saved_csv_save_file == None
+                        log_pd = pd.read_csv(saved_csv_save_file, header = 0)  
+                        self.initial_epoch = 1 + log_pd['epoch'].iloc[-1]  
+                    else:
+                        print('Saved CSV file not found or was invalid with size = 0 bytes. New CSV file will be created with staring epoch = 1')
                     self.model_loaded_sucessfully = True
                     print('>>> Previously saved model found. Training will continue from file: \n' + saved_model_filename) 
                 except OSError:
@@ -92,7 +95,7 @@ class model_load_and_save:
         
         self.model_save_file = model_save_directory + '/' + 'weights-{epoch:02d}-{val_loss:.2f}.h5'
         #self.csv_save_file = model_save_directory + '/' + 'log-' + str(self.initial_epoch) + '.csv'
-        self.csv_save_file = model_save_directory + '/' + 'training_log'
+        self.csv_save_file = model_save_directory + '/' + 'training_log.csv'
 
         self.tensorboard_log_dir = model_save_directory
        
